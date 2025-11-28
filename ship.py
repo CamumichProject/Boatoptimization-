5ee7541a341f5c2c143c45755e0666ef8d20855b
irosey5 <irosey@umich.edu>
2025-11-28 14:26:04 -0500

    Added wake fraction & series 60 paper

--- ship.py
+++ ship.py
@@ -1,4 +1,5 @@
 from physics import residual_resistance_coef, froude_number, reynolds_number, frictional_resistance_coef
+import numpy as np
 
 
 class Ship:
@@ -8,7 +9,7 @@ class Ship:
 
     def __init__(self, length: float, draught: float, beam: float, speed: float,
                  slenderness_coefficient: float, prismatic_coefficient: float,
-                 CM: float, lcb: float) -> None:
+                 CM: float, lcb: float, prop_diameter: float) -> None:
         """
         Assign values for the main dimension of a ship.
 
@@ -34,12 +35,11 @@ class Ship:
         self.CM = CM
         self.surface_area = 1.7 * self.length * self.draught + self.displacement / self.draught
         self.lcb = lcb
-        self.LRL = 1 - self.CP + 0.06 * self.CP * self.lcb / (4 * self.CP - 1)
-        
-        self.c12 = 
-        
-        
-        self.onePlusk1 =
+        self.LR = (1 - self.CP + 0.06 * self.CP * self.lcb / (4 * self.CP - 1)) * self.length
+        self.c12 = self.calc_c12()
+        self.onePlusk1 = 0.93 + self.c12 * (self.beam / self.LR )**0.92497 * (0.95 - self.CP) ** -0.521448 * (1 - self.CP + 0.0225 * self.lcb) ** 0.6906
+        self.prop_diameter = prop_diameter
+
 
 
 
@@ -55,6 +55,12 @@ class Ship:
                                                          self.CP,
                                                          froude_number(self.speed, self.length))
         return 1 / 2 * total_resistance_coef * 1025 * self.surface_area * self.speed ** 2
+
+    def resistance_coeff(self):
+        return frictional_resistance_coef(self.length, self.speed) + \
+                                residual_resistance_coef(self.slenderness_coefficient,
+                                                         self.CP,
+                                                         froude_number(self.speed, self.length))
 
     def maximum_deck_area(self, water_plane_coef: float = 0.88) -> float:
         """
@@ -85,8 +91,51 @@ class Ship:
         return (1 + sea_margin) * self.resistance * self.speed / propulsion_eff
 
     def wake_fraction(self) -> float:
-        c8 = self.beam * self.surface_area
-        return 1
+        c9 = self.calc_c9()
+        CV = self.calc_CV()
+        c11 = self.calc_c11()
+        CP1 = 1.45 * self.CP - 0.315 - 0.0225 * self.lcb
 
-    def c12(self) -> float:
-        return 1
+        term1 = c9 * CV * self.length / self.draught * (0.661875 + 1.21756 * c11 * CV / (1 - CP1))
+        term2 = 0.24588 * np.sqrt(self.beam / (self.length * (1 - CP1)))
+        term3 = -0.09726 / (0.95 - self.CP)
+        term4 = 0.11434 / (0.95 - self.CB)
+        
+        return term1 + term2 + term3 + term4
+
+    def calc_c12(self) -> float:
+        if self.draught / self.length > 0.05:
+            return (self.draught / self.length) ** 0.2228446
+        
+        elif self.draught / self.length > 0.02:
+            return 48.2 * (self.draught / self.length - 0.02) ** 2.078 + 0.479948
+        
+        else:
+            return 0.479948
+        
+    def calc_CV(self) -> float:
+        Re = reynolds_number(self.length, self.speed) 
+        # From ITTC 1957 Friction Line
+        CF = 0.075 / ((np.log10(Re) - 2)**2)
+        CA = 0.006 * (self.length + 100) ** (-0.16) - 0.00205
+        return (self.onePlusk1 * CF) + CA
+
+    def calc_c9(self) -> float:
+        if self.beam / self.draught < 5:
+            c8 = self.beam * self.surface_area / (self.length * self.prop_diameter * self.draught)
+        
+        else:
+            c8 = self.surface_area * (7 * self.beam / self.draught - 25) / (self.length * self.prop_diameter * (self.beam / self.draught - 3))
+
+        if c8 < 28:
+            return c8
+        else:
+            return 32 - 16 / (c8 - 24)
+        
+    def calc_c11(self) -> float:
+        if self.draught / self.prop_diameter < 2:
+            return self.draught / self.prop_diameter
+        else:
+            return 0.0833333 * (self.draught / self.prop_diameter) + 1.333333
+
+
